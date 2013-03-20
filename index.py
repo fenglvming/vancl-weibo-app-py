77ecc3ced00a14f00d190f0c76affa131523314b
fenglvming <fenglvming@gmail.com>
2013-03-20 17:32:50 +0800

    update1

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import web
+from view import render
 urls = ('/','index')
-  
+
 class index:
     def GET(self):
-        return "hello world!"
+        return render.index('fenglvming')
   
 app = web.application(urls, globals(), autoreload=False)
+app.internalerror = web.debugerror
 application = app.wsgifunc()
