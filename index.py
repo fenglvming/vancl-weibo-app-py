3f9f0cddd7e5a15eaee15842b609f8328a5e0957
fenglvming <fenglvming@gmail.com>
2013-03-20 18:04:36 +0800

    update2

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
 import web
+import os,sys
+abspath = os.path.dirname(__file__)
+sys.path.append(abspath)
+os.chdir(abspath)
+import view
 from view import render
 urls = ('/','index')
 
+
 class index:
     def GET(self):
-        return render.index('fenglvming')
+        return render.index()
   
 app = web.application(urls, globals(), autoreload=False)
 app.internalerror = web.debugerror
